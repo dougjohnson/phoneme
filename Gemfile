ce890f510191ca466798420243caf976601bfdb0
Doug Johnson doug.johnson@sage.com <doug.johnson@sage.com>
2011-12-19 20:22:56 +0000

    Added functionality and tests

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'http://rubygems.org' 
+
+gem 'sinatra'
+gem 'builder'
+gem 'twilio-ruby'
+
+group :test do
+  gem 'rspec'
+  gem 'rack-test'
+end
--- app.rb
+++ app.rb
@@ -1,11 +1,35 @@
 require 'sinatra'
 require 'builder'
+require 'twilio-ruby'
+
+def numbers
+  {ENV['DOUG_MOBILE'] => 'Doug',
+   ENV['MARCUS_MOBILE'] => 'Marcus',
+   ENV['ED_MOBILE'] => 'Ed',
+   ENV['KEITH_MOBILE'] => 'Keith'}
+end
+
+post '/call-handler' do
+  logger.info params
+  response = Twilio::TwiML::Response.new do |r|
+    r.Say 'Hello, '+numbers[params['Called']]+'. The Sage One application has stopped responding. This phone call has been triggered as a result of ping dom being unable to contact the site for three consecutive minutes. Goodbye!', :voice => 'man', :language => 'en-gb'
+  end
+  return response.text
+end
 
 post '/' do
-  builder do |xml|
-    xml.instruct!
-    xml.Response do 
-      xml.Say("Hello from my Heroku app")
+  numbers.each do |number, name|
+    if params[:plain] =~ /call #{name}/i
+      @client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
+      @call = @client.account.calls.create(
+        :from => ENV['DOUG_MOBILE_WORK'],
+        :to => number,
+        :url => 'http://phoneme.heroku.com/call-handler'
+      )
     end
   end
+end
+
+get '/' do
+  "here there be dragons..."
 end
