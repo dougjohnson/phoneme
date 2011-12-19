f90f0c82802cf1a5a32330921da2ffe170d7667d
Doug Johnson doug.johnson@sage.com <doug.johnson@sage.com>
2011-12-19 21:31:15 +0000

    added twilio

--- Gemfile
+++ Gemfile
@@ -2,3 +2,9 @@ source 'http://rubygems.org'
 
 gem 'sinatra'
 gem 'builder'
+gem 'twilio-ruby'
+
+group :test do
+  gem 'rspec'
+  gem 'rack-test'
+end
--- app.rb
+++ app.rb
@@ -1,11 +1,25 @@
 require 'sinatra'
 require 'builder'
+require 'twilio-ruby'
 
-get '/' do
-  "Hello"
+account_sid = 'AC773b9ae797d841da8cf4a6d7e0e4f0fe'
+auth_token = 'bfbb4e65cf5fb819ed53fb36ab6b80a9'
+
+get '/call-handler' do
+  response = Twilio::TwiML::Response.new do |r|
+    r.Say 'you are very very gay', :voice => 'man', :language => 'en-gb'
+  end
 end
 
 post '/' do
+  
+  if params[:plain] =~ /call doug/
+    @client = Twilio::REST::Client.new account_sid, auth_token
+    @call = @client.account.calls.create(
+      :from => '+447973157563',
+      :to => '+447879624700',
+      :url => 'http://phoneme.heroku.com/call-handler'
+    )
+  end
 
-  #logger.info 'Here is the body:' + params['plain']
 end
