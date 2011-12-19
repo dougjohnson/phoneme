73a701c8a72c3084bef2e7f37c9e5d19a5d177ab
Doug Johnson doug.johnson@sage.com <doug.johnson@sage.com>
2011-12-19 22:30:45 +0000

    added hash of names and numbers

--- app.rb
+++ app.rb
@@ -5,25 +5,29 @@ require 'twilio-ruby'
 account_sid = 'AC773b9ae797d841da8cf4a6d7e0e4f0fe'
 auth_token = 'bfbb4e65cf5fb819ed53fb36ab6b80a9'
 
+numbers = {'+447879624700' => 'Doug',
+           '+447977100812' => 'Marcus'}
+
+
 post '/call-handler' do
   logger.info params
   response = Twilio::TwiML::Response.new do |r|
-    r.Say 'Hello, Marcus. You are very very gay.', :voice => 'man', :language => 'en-gb'
-    r.Say 'No, Marcus is lovely. And I assure you he is definitely not a homosexual.', :voice => 'woman'
-    r.Say 'Who the hell are you?', :voice => 'man', :language => 'en-gb'
-    r.Say 'I am his teddy bear. Didn\'t you know I\'m a girl?', :voice => 'woman'
+    r.Say 'Hello, '+numbers[params['Called']]+'. You are very very gay.', :voice => 'man', :language => 'en-gb'
   end
   return response.text
 end
 
 post '/' do
-  if params[:plain] =~ /call doug/i
-    @client = Twilio::REST::Client.new account_sid, auth_token
-    @call = @client.account.calls.create(
-      :from => '+447973157563',
-      :to => '+447879624700',
-      :url => 'http://phoneme.heroku.com/call-handler'
-    )
+
+  numbers.each do |number, name|
+    if params[:plain] =~ /call #{name}/i
+      @client = Twilio::REST::Client.new account_sid, auth_token
+      @call = @client.account.calls.create(
+        :from => '+447973157563',
+        :to => number,
+        :url => 'http://phoneme.heroku.com/call-handler'
+      )
+    end
   end
 
   if params[:plain] =~ /call marcus/i
@@ -31,11 +35,6 @@ post '/' do
     @call = @client.account.calls.create(
       :from => '+447973157563',
       :to => '+447879624700',
-      :url => 'http://phoneme.heroku.com/call-handler'
-    )
-    @call = @client.account.calls.create(
-      :from => '+447973157563',
-      :to => '+447977100812',
       :url => 'http://phoneme.heroku.com/call-handler'
     )
   end
