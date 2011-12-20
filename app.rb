b0026dcab4b6c11e7516cd92484704b25fc689e8
Doug Johnson doug.johnson@sage.com <doug.johnson@sage.com>
2011-12-20 21:07:48 +0000

    modified the message

--- app.rb
+++ app.rb
@@ -12,13 +12,12 @@ numbers = {'+447879624700' => 'Doug',
 post '/call-handler' do
   logger.info params
   response = Twilio::TwiML::Response.new do |r|
-    r.Say 'Hello, '+numbers[params['Called']]+'. You are very very gay.', :voice => 'man', :language => 'en-gb'
+    r.Say 'Hello, '+numbers[params['Called']]+'. The Sage One application has stopped responding. This phone call has been triggered as a result of pingdom being unable to contact the site for three consecutive minutes.', :voice => 'man', :language => 'en-gb'
   end
   return response.text
 end
 
 post '/' do
-
   numbers.each do |number, name|
     if params[:plain] =~ /call #{name}/i
       @client = Twilio::REST::Client.new account_sid, auth_token
@@ -28,14 +27,5 @@ post '/' do
         :url => 'http://phoneme.heroku.com/call-handler'
       )
     end
-  end
-
-  if params[:plain] =~ /call marcus/i
-    @client = Twilio::REST::Client.new account_sid, auth_token
-    @call = @client.account.calls.create(
-      :from => '+447973157563',
-      :to => '+447879624700',
-      :url => 'http://phoneme.heroku.com/call-handler'
-    )
   end
 end
